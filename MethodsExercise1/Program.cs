7d8b20ac8b4f640d5674908ae1b346ee9528c58a
bryan mcwatters <bmcwatters@comcast.net>
2021-12-04 18:02:02 -0500

    second commit

--- MethodsExercise1/Program.cs
+++ MethodsExercise1/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("----------");
+﻿
 Console.WriteLine("What is your name?");
 var firstName = Console.ReadLine();
 Console.WriteLine("Hello " + firstName);
@@ -15,12 +13,15 @@ string band = Console.ReadLine();
 Console.WriteLine("Cool, I also like the " + band);
 Console.WriteLine($"Nice to meet you {firstName}, I also like {color}. My favorite animal is also a {animal}");
 
+
 Console.WriteLine("Give me a number to add");
 int num1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Give me a second number to add");
 int num2 = int.Parse(Console.ReadLine());
 int sum = Sum(num1, num2);
 Console.WriteLine($"The sum is: {sum}");
+
+
 Console.WriteLine("Give me a number to multiply");
 int x = int.Parse(Console.ReadLine());
 Console.WriteLine("Give me a second number to multiply");
@@ -28,6 +29,27 @@ int y = int.Parse(Console.ReadLine());
 int multiply = Multiply(x, y);
 Console.WriteLine($"The product is: {multiply}");
 
+
+Console.WriteLine("Give me a number to subtract");
+int t = int.Parse(Console.ReadLine());
+Console.WriteLine("Give me a second number to subtract");
+int v = int.Parse(Console.ReadLine());
+int subtract = Subtract(t, v);
+Console.WriteLine($"The product is: {subtract}");
+
+Console.WriteLine("Give me a number to divide");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Give me a second number to divide");
+int n = int.Parse(Console.ReadLine());
+int divide = Divide(m, n);
+Console.WriteLine($"The product is: {divide}");
+
+Console.WriteLine("Give me a number to find the remainder");
+int c = int.Parse(Console.ReadLine());
+Console.WriteLine("Give me a second number to find the remainder");
+int d = int.Parse(Console.ReadLine());
+int remainder = Modulus(c, d);
+Console.WriteLine($"The product is: {remainder}");
 
 static int Sum(int num1, int num2)
     {
@@ -37,3 +59,18 @@ static int Sum(int num1, int num2)
     {
         return x * y;
     }
+static int Subtract(int t, int v)
+{
+    return t - v;
+
+}
+static int Divide(int m, int n)
+{
+    return m / n;
+
+}
+static int Modulus(int c, int d)
+{
+    return c % d;
+
+}
